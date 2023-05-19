97c5ea763d9cac3a777d395a765b47c0f46f7934
Cesar Cipher <danielakastrejon@gmail.com>
2023-05-19 10:22:23 -0600

    añadiendo comentario

--- src/cipher.js
+++ src/cipher.js
@@ -1,40 +1,67 @@
-const cipher = { 
-    
-    encode:(texto, desplazamiento)=>{
-        let resultado = "";
-        let numDesplaza = Number(desplazamiento);
-        let ubicaLetra = texto.charCodeAt(0)
-                
-        for (let i= 0; i <texto.length; i++) {
-          let vuelta = texto.charCodeAt(i) + numDesplaza
-          let cifra = String.fromCharCode(vuelta);
-          console.log(cifra)
-          resultado += cifra
+const cipher = {
 
+  encode: (desplazamiento, texto) => {
+    let resultado = "";
+    const numDesplaza = Number(desplazamiento);
+    if (typeof parseInt(desplazamiento) !== "number" || typeof texto !== "string") {
+      throw new TypeError(
+        "El texto tiene que ser un string y el desplazamiento un numero");
+    } else {
+      for (let i = 0; i < texto.length; i++) {
+        let cifra = "";
+        console.log(texto.charCodeAt(i) ==32);
+        if ( texto.charCodeAt(i) == 32){
+          cifra = " ";
+          } else {
+        const vuelta = (texto.charCodeAt(i) + numDesplaza - 65) % 26 + 65;
+          cifra = String.fromCharCode(vuelta);
+        console.log(cifra)
         }
-        return resultado
+        resultado += cifra
 
-
-    },
-    decode: (texto, desplazamiento)=>{
-        let resultado2 = "";
-        let numDesplaza = Number(desplazamiento);
-        let ubicaLetra = texto.charCodeAt(0)
-                
-        for (let i= 0; i <texto.length; i++) {
-          let vuelta = texto.charCodeAt(i) - numDesplaza
-          let cifra = String.fromCharCode(vuelta);
-          console.log(cifra)
-          resultado2 += cifra
-
-        }
-        console.log(resultado2)
-
-   
+      }
+      return resultado
     }
 
+  },
+  decode: (desplazamiento, texto) => {
+    //console.log({texto})
+    let resultado2 = "";
+    const numDesplaza = Number(desplazamiento);
+    if (typeof parseInt(desplazamiento) !== "number" || typeof texto !== "string") {
+      throw new TypeError(
+        "El texto tiene que ser un string y el desplazamiento un numero");
+    } else {
+      for (let i = 0; i < texto.length; i++) {
+        let vuelta; 
+        //console.log({i})
+        if ( texto.charCodeAt(i) === 32){
+          vuelta = 32 ;
+        }
+        if (texto.charCodeAt(i) - desplazamiento -65>0){
+          vuelta = (texto.charCodeAt(i) - numDesplaza)
+          console.log("Mayor que 0")
+        }  
+        if (((texto.charCodeAt(i) - desplazamiento - 65) %26)*-1  ===0){
+          vuelta = 65;
+          console.log("Igual a 0")
+        }
+        else if (texto.charCodeAt(i) - desplazamiento -65<=0){
+          vuelta = (texto.charCodeAt(i) - numDesplaza - 65) % 26 + 65 +26;  
+          console.log("Mayor que 0")
+        } 
+        const cifra = String.fromCharCode(vuelta);
+        //console.log(cifra)
+        resultado2 += cifra
 
-    
+      }
+      return resultado2
+    }
+
+  }
+
+
+
 
 }
 
@@ -42,43 +69,3 @@ const cipher = {
 
 
 export default cipher;
-
-
-
-/*
-encode:(texto, desplazamiento) => {
-
-    Number(desplazamiento)
-    //console.log ("Number" + " Creo que este es un DOM")
-
-    document.getElementById("desplazamiento")
-    //console.log(" desplazamiento" + " este es del DOM")//
-   
-    document.getElementById("mensaje")
-    //console.log("mensaje" +" este es del DOM")//
-
-    document.getElementById("mensaje2")
-    //console.log("mensaje2" + " este es del dom")//
-
-    //let cipher = "mensaje"//
-
-    console.log(texto, desplazamiento);
-    console.log(texto.charCodeAt(0) + Number(desplazamiento));
-
-    const cadenaDeNombre = texto;
-
-    console.log(cadenaDeNombre.length);
-
-    for (let i= 0; i <cadenaDeNombre.length; i++) {
-        console.log(cadenaDeNombre[i], cadenaDeNombre.charCodeAt(i)) 
-          
-    }
-
-    document.getElementById("mensaje2") 
-    //console.log("mensaje2" + "Esto tambíen creo que es parte del DOM");//
-    //*console.log(desplazamiento + " este es del DOM")
-    document.getElementById("mensaje2").value = cipher(texto, desplazamiento);
-    console.log(document.getElementById("mensaje2").value = cipher(texto, desplazamiento));*/
-    /*document.getElementById("desplazamiento")= Number
-
-} */
--- src/index.js
+++ src/index.js
@@ -1,80 +1,34 @@
 import cipher from './cipher.js';
 
-//console.log(cipher);
 
-
-/*
- //Esto es para que todo lo que se escriba en minusculas se haga mayusculas//
-function inicio(){
-   
-    document.getElementById("mensaje").addEventListener("keyup", function(){
-        this.value = this.value.toUpperCase();
-        console.log(this.value);
-    },true);
-    
-    //Que la funcion regrese el mesaje escrito//
-    document.getElementById("cifrar").addEventListener("click", function(){
-        let texto = document.getElementById("mensaje").value;
-        let desplazamiento = document.getElementById("desplazamiento").value;
-        document.getElementById("mensaje2").value = cifrar(texto, desplazamiento);
-    }, true);
-
-    document.getElementById("descifrar").addEventListener("click", function(){
-        let texto = document.getElementById("mensaje").value;
-        let desplazamiento = document.getElementById("desplazamiento").value;
-        document.getElementById("mensaje2").value = descifrar(texto, desplazamiento)
-    },true);
-
-}
-
- //Hacer el cifrado y el descifrado creo que esta parte se hace en el cipher js//
-
- function cifrar(texto, desplazamiento){
-
-    
-        
- }*/
-
- document.getElementById("mensaje").addEventListener("keyup", function(){
-    this.value = this.value.toUpperCase();
-    //console.log(this.value);
+document.getElementById("mensaje").addEventListener("keyup", function () {
+  this.value = this.value.toUpperCase();
+  //console.log(this.value);
 });
 
-document.getElementById("cifrar").addEventListener("click", function(){
-    let texto = document.getElementById("mensaje").value;
-    let desplazamiento = document.getElementById("desplazamiento").value;
-    let textoCifrado = document.getElementById("mensaje2");
-    console.log(cipher.encode(texto, desplazamiento));
-    textoCifrado.value = cipher.encode(texto, desplazamiento);
+document.getElementById("cifrar").addEventListener("click", function () {
+  const texto = document.getElementById("mensaje").value;
+  const desplazamiento = document.getElementById("desplazamiento").value;
+  const textoCifrado = document.getElementById("mensaje2");
+  console.log(cipher.encode(desplazamiento, texto));
+  textoCifrado.value = cipher.encode(desplazamiento, texto);
 
 
-    
-   
+
+
 });
 
-document.getElementById("descifrar").addEventListener("click", function(){
-    let texto = document.getElementById("mensaje").value;
-    let desplazamiento = document.getElementById("desplazamiento").value;
-    let textoCifrado = document.getElementById("mensaje2").value;
-    cipher.decode(texto, desplazamiento, textoCifrado);
+document.getElementById("descifrar").addEventListener("click", function () {
+  const texto = document.getElementById("mensaje").value;
+  const desplazamiento = document.getElementById("desplazamiento").value;
+  //cipher.decode(texto, desplazamiento, textoCifrado);
+  document.getElementById("mensaje2").value = cipher.decode(desplazamiento,texto);
+
 
 
 })
 
 
- //const cifrar = document.querySelector('#cifrar');
- //const descifrar = document.querySelector('#descifrar');
 
- //descifrar.addEventListener('click', e => {
-
- //})
-
-
-document.getElementById("cifrar").addEventListener("click", function(){
-    console.log("prueba")
-})
- //console.log(resultado + "prueba");
-
- //document.getElementById("mensaje2").innerHTML = resultado ;
 
 
