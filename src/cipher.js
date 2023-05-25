a07c25714b285751b4712ca2d3ade50f564a86b5
Cesar Cipher <danielakastrejon@gmail.com>
2023-05-24 19:07:28 -0600

    cambio de cipher

--- src/cipher.js
+++ src/cipher.js
@@ -9,13 +9,13 @@ const cipher = {
     } else {
       for (let i = 0; i < texto.length; i++) {
         let cifra = "";
-        console.log(texto.charCodeAt(i) ==32);
-        if ( texto.charCodeAt(i) == 32){
+        console.log(texto.charCodeAt(i) === 32);
+        if (texto.charCodeAt(i) === 32) {
           cifra = " ";
-          } else {
-        const vuelta = (texto.charCodeAt(i) + numDesplaza - 65) % 26 + 65;
+        } else {
+          const vuelta = (texto.charCodeAt(i) + numDesplaza - 65) % 26 + 65;
           cifra = String.fromCharCode(vuelta);
-        console.log(cifra)
+          console.log(cifra)
         }
         resultado += cifra
 
@@ -33,23 +33,23 @@ const cipher = {
         "El texto tiene que ser un string y el desplazamiento un numero");
     } else {
       for (let i = 0; i < texto.length; i++) {
-        let vuelta; 
+        let vuelta;
         //console.log({i})
-        if ( texto.charCodeAt(i) - desplazamiento +65>32){
+        if (texto.charCodeAt(i) - desplazamiento + 65 > 32) {
           vuelta = " ";
         }
-        if (texto.charCodeAt(i) - desplazamiento -65>0){
+        if (texto.charCodeAt(i) - desplazamiento - 65 > 0) {
           vuelta = (texto.charCodeAt(i) - numDesplaza)
           console.log("Mayor que 0")
-        }  
-        if (((texto.charCodeAt(i) - desplazamiento - 65) %26)*-1  ===0){
+        }
+        if (((texto.charCodeAt(i) - desplazamiento - 65) % 26) * -1 === 0) {
           vuelta = 65;
           console.log("Igual a 0")
         }
-        else if (texto.charCodeAt(i) - desplazamiento -65<=0){
-          vuelta = (texto.charCodeAt(i) - numDesplaza - 65) % 26 + 65 +26;  
+        else if (texto.charCodeAt(i) - desplazamiento - 65 <= 0) {
+          vuelta = (texto.charCodeAt(i) - numDesplaza - 65) % 26 + 65 + 26;
           console.log("Mayor que 0")
-        } 
+        }
         const cifra = String.fromCharCode(vuelta);
         //console.log(cifra)
         resultado2 += cifra
